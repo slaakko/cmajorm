2d24c6b44a895a3830624b975a8900276904bd29
Seppo Laakko <slaakko@gmail.com>
2019-06-17 22:31:18 +0300

    cmsx mv rmdir

---
+++ cmajor/projects/cmsx/build/installd.bat
@@ -0,0 +1,13 @@
+@echo off
+xcopy /Y /D coreutil\cat\bin\debug ..\bind
+xcopy /Y /D coreutil\chmod\bin\debug ..\bind
+xcopy /Y /D coreutil\cp\bin\debug ..\bind
+xcopy /Y /D coreutil\echo\bin\debug ..\bind
+xcopy /Y /D coreutil\fc\bin\debug ..\bind
+xcopy /Y /D coreutil\ls\bin\debug ..\bind
+xcopy /Y /D coreutil\mkdir\bin\debug ..\bind
+xcopy /Y /D coreutil\mv\bin\debug ..\bind
+xcopy /Y /D coreutil\passwd\bin\debug ..\bind
+xcopy /Y /D coreutil\rm\bin\debug ..\bind
+xcopy /Y /D coreutil\rmdir\bin\debug ..\bind
+xcopy /Y /D coreutil\umask\bin\debug ..\bind
--- cmajor/projects/cmsx/build/reinstall.bat
+++ cmajor/projects/cmsx/build/reinstall.bat
@@ -5,6 +5,9 @@ xcopy /Y coreutil\cp\bin\release ..\bin
 xcopy /Y coreutil\echo\bin\release ..\bin
 xcopy /Y coreutil\ls\bin\release ..\bin
 xcopy /Y coreutil\mkdir\bin\release ..\bin
+xcopy /Y coreutil\mv\bin\release ..\bin
 xcopy /Y coreutil\passwd\bin\release ..\bin
+xcopy /Y coreutil\rm\bin\release ..\bin
+xcopy /Y coreutil\rmdir\bin\release ..\bin
 xcopy /Y coreutil\umask\bin\release ..\bin
 xcopy /Y proc\sync\bin\release ..\sbin
