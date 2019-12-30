db691778a21f9648e59bed240262df6ccb30364d
Seppo Laakko <slaakko@gmail.com>
2019-12-30 20:21:19 +0200

    test

---
+++ cmajor/rt/WindowsAPI.cpp
@@ -0,0 +1,94 @@
+// =================================
+// Copyright (c) 2019 Seppo Laakko
+// Distributed under the MIT license
+// =================================
+
+#include <cmajor/rt/WindowsAPI.hpp>
+#include <Windows.h>
+
+uint64_t WinGetLastError()
+{
+    return GetLastError();
+}
+
+void WinFormatMessage(uint64_t errorCode, char* buffer)
+{
+    FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, errorCode, LANG_SYSTEM_DEFAULT, buffer, 4096, nullptr);
+}
+
+void* WinCreateEvent()
+{
+    HANDLE handle = CreateEventA(NULL, false, false, NULL);
+    return handle;
+}
+
+bool WinSetEvent(void* eventHandle)
+{
+    bool retval = SetEvent(eventHandle);
+    return retval;
+}
+
+bool WinResetEvent(void* eventHandle)
+{
+    bool retval = ResetEvent(eventHandle);
+    return retval;
+}
+
+bool WinWaitEvent(void* eventHandle)
+{
+    DWORD retval = WaitForSingleObject(eventHandle, INFINITE);
+    return retval == WAIT_OBJECT_0;
+}
+
+bool WinCloseHandle(void* handle)
+{
+    bool retval = CloseHandle(handle);
+    return retval;
+}
+
+int32_t WinWaitForMultipleObjects(uint32_t count, void** handles)
+{
+    uint32_t retval = WaitForMultipleObjects(count, handles, false, INFINITE);
+    if (retval == WAIT_FAILED)
+    {
+        return -1;
+    }
+    else if (retval == WAIT_TIMEOUT)
+    {
+        return -2;
+    }
+    else if (retval >= WAIT_OBJECT_0 && retval < WAIT_OBJECT_0 + count)
+    {
+        return retval - WAIT_OBJECT_0;
+    }
+    else if (retval >= WAIT_ABANDONED_0 && retval < WAIT_ABANDONED_0 + count)
+    {
+        return -3;
+    }
+    else
+    {
+        return -4;
+    }
+}
+
+void* WinFindFirstChangeNotification(const char* directoryPath)
+{
+    void* handle = FindFirstChangeNotificationA(directoryPath, false, FILE_NOTIFY_CHANGE_FILE_NAME);
+    if (handle == INVALID_HANDLE_VALUE)
+    {
+        return nullptr;
+    }
+    return handle;
+}
+
+bool WinFindNextChangeNotification(void* handle)
+{
+    bool retval = FindNextChangeNotification(handle);
+    return retval;
+}
+
+bool WinFindCloseChangeNotification(void* handle)
+{
+    bool retval = FindCloseChangeNotification(handle);
+    return retval;
+}
---
+++ cmajor/rt/WindowsAPI.hpp
@@ -0,0 +1,24 @@
+// =================================
+// Copyright (c) 2019 Seppo Laakko
+// Distributed under the MIT license
+// =================================
+
+#ifndef CMAJOR_RT_WINDOWS_API_INCLUDED
+#define CMAJOR_RT_WINDOWS_API_INCLUDED
+#include <cmajor/rt/RtApi.hpp>
+#include <stdint.h>
+
+extern "C" RT_API uint64_t WinGetLastError();
+extern "C" RT_API void WinFormatMessage(uint64_t errorCode, char* buffer);
+extern "C" RT_API void* WinCreateEvent();
+extern "C" RT_API bool WinSetEvent(void* eventHandle);
+extern "C" RT_API bool WinResetEvent(void* eventHandle);
+extern "C" RT_API bool WinWaitEvent(void* eventHandle);
+extern "C" RT_API bool WinCloseHandle(void* handle);
+extern "C" RT_API int32_t WinWaitForMultipleObjects(uint32_t count, void** handles);
+extern "C" RT_API void* WinFindFirstChangeNotification(const char* directoryPath);
+extern "C" RT_API bool WinFindNextChangeNotification(void* handle);
+extern "C" RT_API bool WinFindCloseChangeNotification(void* handle);
+
+#endif // CMAJOR_RT_WINDOWS_API_INCLUDED
+
