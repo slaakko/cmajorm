b475a0d9cd1353225573356bf8b7071a4b69f1c6
slaakko <slaakko@gmail.com>
2020-10-26 22:37:43 +0200

    unit tests

---
+++ cmajor/unitTest/cmajor/run.bat
@@ -0,0 +1,3 @@
+@echo off
+cmunit --verbose cmajor.cms
+cmunit --verbose --config=release cmajor.cms
---
+++ devenv/master.util/dbg/LogFile.cs
@@ -0,0 +1,47 @@
+﻿/*========================================================================
+    Copyright (c) 2011-2012 Seppo Laakko
+    http://sourceforge.net/projects/soulparsing/
+ 
+    Distributed under the GNU General Public License, version 3 (GPLv3).
+    (See accompanying LICENSE.txt or http://www.gnu.org/licenses/gpl.html)
+
+========================================================================*/
+
+using System;
+using System.IO;
+
+namespace master.util.debug
+{
+    public class LogFile
+    {
+        public LogFile(string logFilePath)
+        {
+            file = new object();
+            this.logFilePath = logFilePath;
+        }
+        public void Write(string text)
+        {
+            try
+            {
+                lock (file)
+                {
+                    using (StreamWriter logWriter = File.AppendText(logFilePath))
+                    {
+                        logWriter.WriteLine(DateTime.Now.ToString() + ": " + text);
+                    }
+                }
+
+            }
+            catch
+            {
+            }
+        }
+        public string Path
+        {
+            get { return logFilePath; }
+            set { logFilePath = value; }
+        }
+        private object file;
+        private string logFilePath;
+    }
+}
