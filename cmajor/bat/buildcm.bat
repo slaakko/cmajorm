d4c00039820c28977f41432f6e128ed1b9264b08
slaakko <slaakko@gmail.com>
2020-11-19 18:32:22 +0200

    bat makefile

--- cmajor/bat/buildcm.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cmc -v ../projects/cm/Cm.cms
-cmc -v -c=release ../projects/cm/Cm.cms
--- cmajor/bat/buildsys.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cmc -v -u --time ../system/platform/windows/System.cms
-cmc -v -u -c=release --time ../system/platform/windows/System.cms
