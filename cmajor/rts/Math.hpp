b2902bd67adcb3e63d5b7dce291788a7c1d267b5
slaakko <slaakko@gmail.com>
2021-01-12 19:06:03 +0200

    sirius

--- cmajor/rts/Math.hpp
+++ cmajor/rts/Math.hpp
@@ -14,8 +14,8 @@ extern "C" RT_API double RtSqrt(double x);
 extern "C" RT_API double RtPi();
 extern "C" RT_API double RtCos(double angleRad);
 extern "C" RT_API double RtSin(double angleRad);
-extern "C" RT_API double RtCos(double angleRad);
-extern "C" RT_API double RtSin(double angleRad);
+extern "C" RT_API double RtArcCos(double x);
+extern "C" RT_API double RtArcSin(double x);
 extern "C" RT_API double RtAtan2(double y, double x);
 extern "C" RT_API double RtEpsilon();
 
