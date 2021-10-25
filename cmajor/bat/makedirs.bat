21fc46cf8e8c5baf0fc985aa21b1adae776d2c32
slaakko <slaakko@gmail.com>
2021-10-25 20:16:19 +0300

    make dirs

---
+++ cmajor/bat/makedirs.bat
@@ -0,0 +1,7 @@
+@echo off
+IF NOT EXIST ..\bin mkdir ..\bin
+IF NOT EXIST ..\lib mkdir ..\lib
+IF NOT EXIST ..\lib\gcc mkdir ..\lib\gcc
+IF NOT EXIST ..\lib\vs mkdir ..\lib\vs
+IF NOT EXIST ..\rts\build\gcc\bin mkdir ..\rts\build\gcc\bin
+IF NOT EXIST ..\rts\build\gcc\lib mkdir ..\rts\build\gcc\lib
